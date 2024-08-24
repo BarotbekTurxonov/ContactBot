e13d23f140e9db9548dfe106c0e3ee4480707313
Barotbek Turxonov <109985351+BarotbekTurxonov@users.noreply.github.com>
2024-08-24 23:57:10 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -6,10 +6,10 @@ env.read_env()
 
 
 BOT_TOKEN = "7173483653:AAGRrWswyfaFjP_db3CmXVEDbLyakMfoEnk"
-ADMINS = [5235865310]
+ADMINS = [6336526909]
 
 
 # operatorlarni Telegram ID-larini yozing
 support_ids = [
-    5235865310,
+    6336526909,
 ] 
